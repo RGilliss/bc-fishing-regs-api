272c85849c408b4b38c3f22f1bd1377d52a0c267
LB <lrbutler@gmail.com>
2021-04-09 15:36:24 -0700

    Change db pool information

--- server.js
+++ server.js
@@ -11,10 +11,23 @@ const app = express();
 const morgan = require("morgan");
 
 // PG database client/connection setup
-const { Pool } = require("pg");
-const dbParams = require("./lib/db.js");
-const db = new Pool(dbParams);
-db.connect();
+// const { Pool } = require("pg");
+// const dbParams = require("./lib/db.js");
+// const db = new Pool(dbParams);
+// db.connect();
+
+const { Pool } = require('pg');
+const connectionString = process.env.DATABASE_URL
+const db = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+db.connect(() => {
+  console.log('connected to database');
+});
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
