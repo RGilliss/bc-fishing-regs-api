b088e3a1b4fb07439a91829e67ceab18632af84f
LB <lrbutler@gmail.com>
2021-04-09 13:59:33 -0700

    Clean up files

---
+++ db/schema/regs.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS menu CASCADE;
+
+CREATE TABLE regulations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  water_body VARCHAR(255) NOT NULL,
+  class_water BOOLEAN DEFAULT FALSE,
+  tributary BOOLEAN DEFAULT FALSE,
+  stocked BOOLEAN DEFAULT FALSE,
+  accessible BOOLEAN DEFAULT FALSE,
+  date_range VARCHAR(255),
+  regulation TEXT,
+  location POINT
+);
+
